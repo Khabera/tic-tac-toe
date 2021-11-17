3381c7554ec4fa0ebf5d8c95d56645d172e780e8
Shenmew <Cessnator007@gmail.com>
2021-11-16 23:31:36 -0800

    11:30 pm

---
+++ script.js
@@ -0,0 +1,96 @@
+//tic tac toe game
+
+//gameboard -- 9 squares
+//displayController -- Connect to DOM, recieve input?
+
+//I want to get a good understand of the pseudo code before I start this thing, what can I do, and what am I trying to do? Modules allow me to create 
+
+const Gameboard = (function (){
+    let positions = new Array();
+    const rounds = 0;
+    const addRound = (rounds) => {rounds++};
+    for(let i=0; i<9; i++){
+        positions.push('');
+    }
+    const changeState = (value, position) => {
+        console.log('here');
+        if(value == 'x' || value == 'y')
+            positions[position]=value
+        }
+    const numEmptyPositions = () => {
+        let num=0
+        //console.log('eher')
+        positions.forEach(function(position){
+            //console.log('ekekee')
+            if(position==""){
+                //console.log(num);
+                num++
+            }
+        }) 
+        return num;
+    }
+    return {
+        changeState,
+        get positions(){
+            return positions;
+        },
+        addRound,
+        numEmptyPositions
+    }
+})();
+
+const DisplayController = (function(){
+    const displayBoard = document.querySelector('#gameboard');
+    for(let i=0; i<9; i++){
+        let boardSquare = document.createElement('div')
+        boardSquare.classList.add('board-square');
+        boardSquare.setAttribute('number', `${i}`)
+        displayBoard.appendChild(boardSquare);
+        //playerRound toggle
+        boardSquare.addEventListener('click', function(){
+           // console.log(handleGame.onPlayerInput)
+            boardSquare.classList.add(`clicked-player`);
+            handleGame.playerInput(i)
+        
+            //self.setSquare(i) move to a player input function?
+        })        
+    }
+    const changeState = (mark, num, color) => {
+        let square = displayBoard.childNodes[num];
+        square.style.backgroundColor = color;
+        square.textContent=mark;
+    }
+    return{displayBoard,
+    changeState}
+})();
+const handleGame = (function(){
+    const playerInput = (num) => {
+        belf.setSquare(num);
+        getComputerInput()
+    }
+    //Set up as AI later? Build just a two player input
+    //const getComputerInput = () => {
+    //    let num = Math.floor(Math.random() * Gameboard.numEmptyPositions());
+    //    console.log(num);
+    //}
+    return {
+        get onPlayerInput(){
+            return onPlayerInput
+            },
+        playerInput
+    }
+})();
+//
+const PlayerFactory = (name, mark, color) => {
+    const winGame = () => console.log(`The ${name} won the game`);
+    const setSquare = (num) => {
+        Gameboard.changeState(mark, num);
+        DisplayController.changeState(mark, num, color);
+    }
+    return {
+        winGame,
+        setSquare
+    }
+}
+const belf = PlayerFactory('player', 'x', 'green');
+const computer = PlayerFactory('computer', 'o', 'red');
